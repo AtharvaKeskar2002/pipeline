4fd42a2d4b4f9d7f74d072e96ea6a06954ea8a11
AtharvaKeskar2002 <keskaratharva27@gmail.com>
2023-09-30 13:50:15 +0530

    groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,16 +2,19 @@ pipeline {
     agent any
     stages{
         stage('pull'){
+            steps{
             echo "hello pull complete"
+            }
         }
         stage('build'){
+            steps{
             echo "hello build done"
+            }
         }
         stage('test'){
+            steps{
             echo "hello test completed"
-        }
-        stage('deploy'){
-            echo "hello your application is deployed"
+            }
         }
     }
 }
